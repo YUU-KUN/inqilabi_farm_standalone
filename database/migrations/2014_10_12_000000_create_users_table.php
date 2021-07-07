38f100bc31936634c3632ef0c9a9a57b884ec430
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-07-07 23:40:35 +0700

    assign user as default value for user level

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('kecamatan')->nullable();
             $table->string('kota')->nullable();
             $table->string('provinsi')->nullable();
-            $table->enum('level', ['admin', 'user']);
+            $table->enum('level', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
