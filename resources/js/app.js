39e8dbfd4ebbe44e14c0dad7c67c0fbdbe594bf6
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-07-15 09:48:24 +0700

    change baseURL

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,7 +48,7 @@ Vue.filter('formatDate', function(value) {
 
 Vue.config.productionTip = false
 
-axios.defaults.baseURL = 'http://116.193.190.201/api/'
+axios.defaults.baseURL = 'inqilabifarm.com/api/'
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 if (localStorage.getItem("Authorization")) {
