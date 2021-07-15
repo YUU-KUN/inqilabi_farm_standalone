7bc37645c467476e907c5397481b095b02e7d4a7
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-07-15 13:23:37 +0700

    delete baseURL

--- resources/js/app.js
+++ resources/js/app.js
@@ -49,7 +49,6 @@ Vue.filter('formatDate', function(value) {
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'https://inqilabifarm.com/api/'
-// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 if (localStorage.getItem("Authorization")) {
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("Authorization")
